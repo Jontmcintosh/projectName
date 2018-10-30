470da2d5d32e72e7fbe3cb8b154aa9d84b823870
Ubuntu <vagrant@Base-Box>
2018-10-30 03:41:55 +0000

    Added character controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'dogs/index'
+  get 'characters/index'
+  get 'characters/show'
+  get 'dogs', to: 'dogs#index'
+  root to: 'dogs#index'
   get 'dogs/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
