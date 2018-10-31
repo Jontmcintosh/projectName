53bae59ca490c43826adabc32a15998f94734dc3
Jonathan McIntosh <jontmcintosh@hotmail.com>
2018-10-30 19:18:08 -0500

    Added functionality behind

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,9 @@ class SearchController < ApplicationController
     def index
     end
 
-    
+
 
     def results
+        @characters = Character.where('hero LIKE ?', "%#{params[:q]}%")
     end
 end
